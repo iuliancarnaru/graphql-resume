66e1f04dc080549efdacac74801cdefa89b58d37
Iulian Carnaru <iulian.carnaru@gmail.com>
2020-08-13 23:35:47 +0100

    adding data to server

---
+++ src/allTypes/Bio.ts
@@ -0,0 +1,13 @@
+import { objectType } from "@nexus/schema";
+
+export const Bio = objectType({
+  name: "Bio",
+  definition(t) {
+    t.string("name");
+    t.string("tagline");
+    t.string("github");
+    t.string("website");
+    t.string("linkedin");
+    t.string("objective");
+  },
+});
---
+++ src/data.ts
@@ -0,0 +1,33 @@
+export const data = {
+  bio: {
+    name: "Iulian Carnaru",
+    tagline: "Software Engineer",
+    github: "https://github.com/iuliancarnaru",
+    website: "https://iuliancarnaru.uk/",
+    linkedin: "https://www.linkedin.com/in/iulian-carnaru-51aa63108/",
+    objective: "Create great projects",
+  },
+  skills: [""],
+  positions: [
+    {
+      id: "1",
+      title: "",
+      company: "",
+      startDate: "",
+      endDate: null,
+      employmentType: "FULL_TIME",
+      location: "",
+      achivements: [""],
+    },
+    {
+      id: "2",
+      title: "",
+      company: "",
+      startDate: "",
+      endDate: null,
+      employmentType: "FULL_TIME",
+      location: "",
+      achivements: [""],
+    },
+  ],
+};
